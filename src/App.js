7258e09804abbdd9824f8a148efa06968fc2204c
sikderp-github <66568657+sikderp-github@users.noreply.github.com>
2020-08-27 03:02:53 +0600

    update data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import { useState } from 'react';
 import Course from './Component/Courses/Course';
 
 function App() {
-  const [courses, setCourses] = useState([])
+  const [courses, setCourses] = useState([]);
   return (
     <div className="App container-fluid px-2">
       <h1 className="text-center py-4">Welcome to Online Yoga Courses</h1>
-      <Course ></Course>
+      <div className="courseItem">
+        <Course ></Course>
+      </div>
+      <div className="cart">
+        <h3>Courses Summary:</h3>
+        <h5>No. of Courses taken</h5>
+        <h4>Total Course fees:</h4>
+
+      </div>
 
     </div>
   );
--- src/Component/Courses/Course.js
+++ src/Component/Courses/Course.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
+
 const Course = () => {
+
     return (
         <div>
             <div>
